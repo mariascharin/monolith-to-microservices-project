3855a54f9145485272002046941b38ac43f6456b
Maria Scharin <maria.scharin@comvalresearch.com>
2022-11-17 23:57:06 +0100

    Update frontend with live publicproxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adac0d6bad31946609d3d41dcd307709-872259022.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
